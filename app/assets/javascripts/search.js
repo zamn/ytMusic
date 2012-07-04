07f57a0e43446ab4b9dcd9fc84caf81bfb81eaaa
Adam Hamot <Rjhoops92@gmail.com>
2012-07-04 16:02:07 -0400

    Implemented forward/back buttons which now work with Shuffle only.

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,53 +1,99 @@
-var shuffle = false;
 var fstack = [];
 var pstack = [];
+var videos = [];
+var temp = -1;
+var player;
+var shuffle = (function() { return $('#shuffle').is(":checked"); });
 
 $.ajaxSetup ({
   cache: false
 });
 
-function nextSong() {
-  $.getJSON('music.json', null, function(data) {
-    var rand = Math.floor(Math.random()*data);
-    var song = $("#video-" + rand);
-    var vidurl = $.trim(song.siblings(".videourl").text());
-    var videoid = song.attr('id');
-    song.parent().parent().show();
-    console.log(song.siblings());
-    song.siblings("a").ScrollTo({
-      duration: 2000,
-      durationMode: 'all'
-    });
-    song.siblings(".controls").show();
-    player = new YT.Player(videoid, {
-      height: '390',
-      width: '540',
-      videoId: vidurl,
-      events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
-      }
-    });
-    console.log(player);
+function playSong(songNumber) {
+  var song = $("#video-" + songNumber);
+  videos.push(songNumber);
+  var vidurl = $.trim(song.siblings(".videourl").text());
+  var videoid = song.attr('id');
+  song.parent().parent().show();
+  song.siblings("a").ScrollTo({
+    duration: 2000,
+    durationMode: 'all'
   });
+  song.siblings(".controls").show();
+  player = new YT.Player(videoid, {
+    height: '390',
+    width: '540',
+    videoId: vidurl,
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function nextSong() {
+  if (fstack.length > 0) {
+    pstack.push(temp);
+    temp = fstack.pop();
+    playSong(temp);
+  }
+  else {
+    $.getJSON('music.json', null, function(data) {
+      var rand = Math.floor(Math.random()*data);
+      var song = $("#video-" + rand);
+      if (temp == -1) {
+        temp = rand;
+      }
+      else {
+        pstack.push(temp);
+        temp = rand;
+      }
+      playSong(rand);
+    });
+  }
+}
+
+function prevSong() {
+  if (pstack.length > 0) {
+    var prevSong = pstack.pop();
+    playSong(prevSong);
+    fstack.push(temp);
+    temp = prevSong;
+  }
+}
+
+function removePlayers() {
+  if (videos.length != 0) {
+    for (var i = 0; i < videos.length; i++) {
+      var iframe = $('iframe[id="video-' + videos[i] + '"]');
+      iframe.siblings(".controls").hide();
+      iframe.parent().parent().hide();
+      iframe.replaceWith("<div id='video-" + videos[i] + "'></div>");
+    }
+  }
 }
 
 function onPlayerReady(event) {
   console.log("ready for blastoff!");
   event.target.playVideo();
+  console.log(event.target.c.id);
 }
 
 function onPlayerStateChange(event) {
   console.log("state changed :o ~ " + event);
+  console.log(event.target.c.id);
 }
 
 $(".back").click(function() {
-  if (shuffle)
-    lastSong();
+  if (pstack.length > 0)
+    removePlayers();
+  if (shuffle())
+    prevSong();
 });
 
 $(".forward").click(function() {
-  if (shuffle)
+  removePlayers();
+  if (shuffle())
     nextSong();
 });
 
@@ -64,18 +110,17 @@ $(".artist").click(function() {
 });
 
 $('#shuffle').click(function() {
-  shuffle = true;
-  if (shuffle)
+  if (shuffle())
     nextSong();
 });
 
 $('#results .titles').on('click', 'a', function(event) {
   event.preventDefault();
-  var vidurl = $(this).siblings(".videourl").text();
-  vidurl = $.trim(vidurl);
+  var vidurl = $.trim($(this).siblings(".videourl").text());
   var video = $(this).siblings('div[id^="video"]');
+  var elemid = video.attr("id");
+  videos.push(elemid.substring(elemid.indexOf("-")+1)); // puts id number on array
   var videoid = video.attr('id');
-console.log(vidurl);
   player = new YT.Player(videoid, {
       height: '390',
       width: '540',
@@ -86,8 +131,6 @@ console.log(vidurl);
       }
   });
   $(this).css("color", "red");
-  console.log(video);
-  video.show();
 });
 
 $('#searchMusic').on('submit',function(ev) { 
